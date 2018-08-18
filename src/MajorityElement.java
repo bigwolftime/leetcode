d4480cf3ec7c7b6378060b2d0a9fcce1a1138a11
XinLiu <zonheng@yahoo.com>
2018-08-18 16:03:30 +0800

    求众数

---
+++ src/MajorityElement.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+/**
+ * 求众数
+ * https://leetcode-cn.com/problems/majority-element/description/
+ * @author bwt
+ *
+ *	思路：
+ *	使用辅助空间 O(N)，循环记录每个数字的次数，办法不是特别好。
+ */
+public class MajorityElement {
+
+	public static int majorityElement(int[] nums) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(nums[i])) {
+				map.put(nums[i], map.get(nums[i]) + 1);
+			}else {
+				map.put(nums[i], 1);
+			}
+			
+			if (map.get(nums[i]) > (nums.length >> 1))
+				return nums[i];
+		}
+		
+        return -1;
+    }
+	
+	public static void main(String[] args) {
+		int[] nums = new int[] {3,2,3};
+		int res = majorityElement(nums);
+		System.err.println(res);
+	}
+}
