88d26a947362ce762e7ffb50d73e255a81107104
XinLiu <zonheng@yahoo.com>
2018-08-21 09:08:00 +0800

    两个数组的交集

--- src/Intersection.java
+++ src/Intersection.java
@@ -13,8 +13,10 @@ public class Intersection {
 		
         for (int i = 0; i < nums1.length; i++) {
         	for (int j = 0; j < nums2.length; j++) {
-        		if (nums1[i] == nums2[j])
-    				hashSet.add(nums1[i]);
+        		if (nums1[i] == nums2[j]) {
+        			hashSet.add(nums1[i]);
+        			break;
+        		}
         	}
         }
         
