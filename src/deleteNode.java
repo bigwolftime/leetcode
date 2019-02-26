ddcd778f73c1c14300ca308c84a2bc205c143de6
XinLiu <zonheng@yahoo.com>
2019-02-26 16:36:18 +0800

    删除链表中的节点

---
+++ src/deleteNode.java
@@ -0,0 +1,21 @@
+import utils.ListNode;
+
+/**
+ * @Author Liuxin
+ * @Date 2019/2/26 15:27
+ *
+ * 删除链表中的节点
+ * https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
+ */
+public class deleteNode {
+
+    public static void deleteNode(ListNode node) {
+        while (node.next.next != null){
+            node.val = node.next.val;
+            node = node.next;
+        }
+
+        node.val = node.next.val;
+        node.next = null;
+    }
+}
