6314faf52eee98a9d48f0e1d3ff89ca4b308af35
XinLiu <zonheng@yahoo.com>
2019-03-26 17:18:28 +0800

    两个数组的交集

--- src/Intersection.java
+++ src/Intersection.java
@@ -1,4 +1,4 @@
-import java.util.HashSet;
+import java.util.*;
 
 /**
  * 两个数组的交集
@@ -9,29 +9,36 @@ import java.util.HashSet;
 public class Intersection {
 
 	public static int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> hashSet = new HashSet<>();
-		
-        for (int i = 0; i < nums1.length; i++) {
-        	for (int j = 0; j < nums2.length; j++) {
-        		if (nums1[i] == nums2[j]) {
-        			hashSet.add(nums1[i]);
-        			break;
-        		}
-        	}
-        }
-        
-        int[] res = new int[hashSet.size()];
-        int index = 0;
-        for (Integer integer : hashSet) {
-			res[index++] = integer;
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> res = new HashSet<>();
+
+        for (int i : nums1){
+        	set.add(i);
 		}
-		return res;
+
+        for (int i : nums2){
+        	if (set.contains(i)){
+        		res.add(i);
+			}
+		}
+
+        int[] num = new int[res.size()];
+        int index = 0;
+		Iterator iterator = res.iterator();
+		while (iterator.hasNext()){
+			num[index++] = (int) iterator.next();
+		}
+
+		return num;
     }
 	
 	public static void main(String[] args) {
-		int[] nums1 = new int[] {4,9,5};
-		int[] nums2 = new int[] {9,4,9,8,4};
-		
-		intersection(nums1, nums2);
+		int[] nums1 = new int[] {1,2,2,1};
+		int[] nums2 = new int[] {2,2};
+
+		int[] res = intersection(nums1, nums2);
+		for (int i : res){
+			System.err.println(i);
+		}
 	}
 }
