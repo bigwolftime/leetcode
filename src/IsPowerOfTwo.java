b6f732974030441e9e863d695ff96085eff37c55
XinLiu <zonheng@yahoo.com>
2019-05-05 10:51:53 +0800

    2的幂

--- src/IsPowerOfTwo.java
+++ src/IsPowerOfTwo.java
@@ -3,10 +3,10 @@
  * @Date 2019/3/13 18:17
  *
  * 2 的幂
- * https://leetcode-cn.com/problems/power-of-two/comments/
+ * https://leetcode-cn.com/problems/power-of-two/
  *
- * 思路：HashMap 源代码中的 tableSizeFor() 方法：传入 n，返回大于等于 n 的 2 的幂次；
- * 另一种思路：只要是 2 的倍数，二进制最高位是 1，其余是 0，即 (n & (n - 1)) == 0
+ * 思路：HashMap 源代码中的 tableSizeFor() 方法：传入 n，返回第 1 个大于等于 n 的 2 的幂次；
+ * 另一种思路：只要是 2 的倍数，二进制最高位是 1，其余是 0，即判断 (n & (n - 1)) == 0
  */
 public class IsPowerOfTwo {
 
@@ -15,7 +15,8 @@ public class IsPowerOfTwo {
             return false;
         }
 
-        return n == tableSizeFor(n);
+        return (n & (n - 1)) ==0;
+        //return n == tableSizeFor(n);
     }
 
     private static int tableSizeFor(int cap){
