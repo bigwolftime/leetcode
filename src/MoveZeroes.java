7925341a49893a51da0077bbc97486db77577b87
XinLiu <zonheng@yahoo.com>
2019-08-19 17:22:25 +0800

    移动零

--- src/MoveZeroes.java
+++ src/MoveZeroes.java
@@ -1,33 +1,43 @@
 /**
+ * 移动零
+ * https://leetcode-cn.com/problems/move-zeroes/
+ *
+ * 思路：使用双指针法，比较简单
  * @Author Liuxin
  * @Date 2019/5/10 16:35
  */
 public class MoveZeroes {
 
     public static void moveZeroes(int[] nums) {
-        if (nums == null || nums.length == 0) {
+        if (nums.length == 0) {
             return;
         }
 
-        int zeroIndex = 0, index = 0;
-        while (zeroIndex < nums.length && index < nums.length) {
-            if (nums[zeroIndex] == 0) {
-                while (index < nums.length) {
-                    if (nums[index] != 0) {
-                        nums[zeroIndex] = nums[index++];
-                        break;
-                    }
-                }
+        int zeroIndex = 0, cur = 0;
+        while (cur < nums.length) {
+            //cur 定位到非 0 元素
+            while (cur < nums.length && nums[cur] == 0){
+                cur++;
             }
-            zeroIndex++;
-            index = index > zeroIndex ? index : zeroIndex;
+
+            if (cur < nums.length){
+                nums[zeroIndex] = nums[cur];
+                cur++;
+                zeroIndex++;
+            }
         }
 
-
+        while (zeroIndex < nums.length){
+            nums[zeroIndex++] = 0;
+        }
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{0, 1, 0, 3, 12};
-        moveZeroes(null);
+        int[] nums = new int[]{0};
+        moveZeroes(nums);
+
+        for (int i : nums){
+            System.err.print(i + ".");
+        }
     }
 }
