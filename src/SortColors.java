ad1008ad218f5000d0d0256bb2778c300d85322c
bwt <zonheng@yahoo.com>
2020-06-10 19:18:36 +0800

    misc: 颜色分类

--- src/SortColors.java
+++ src/SortColors.java
@@ -1,25 +1,39 @@
 /**
+ * 颜色分类
+ * https://leetcode-cn.com/problems/sort-colors/
+ *
  * @author liuxin
  * @time 2020/6/9 20:16
  */
 public class SortColors {
 
     private static void sortColors(int[] nums) {
-        int[] arr = new int[2];
+        int[] arr = new int[3];
 
         for (int i : nums) {
             arr[i]++;
         }
 
-        int count = nums.length;
-        while (count-- > 0) {
+        int idx = 0;
 
+        for (int i = 0; i < arr[0]; i++) {
+            nums[idx++] = 0;
         }
-
+        for (int i = 0; i < arr[1]; i++) {
+            nums[idx++] = 1;
+        }
+        for (int i = 0; i < arr[2]; i++) {
+            nums[idx++] = 2;
+        }
     }
 
     public static void main(String[] args) {
-        sortColors(new int[]{ 2,0,2,1,1,0 });
+        int[] x = new int[]{ 2,0,2,1,1,0 };
+        sortColors(x);
+
+        for (int i : x) {
+            System.err.println(i);
+        }
     }
 
 }
