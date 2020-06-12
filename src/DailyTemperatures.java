b2e556ddb91bc6c557724225ae59120698264ed1
bwt <zonheng@yahoo.com>
2020-06-12 11:28:43 +0800

    Enhancement: 每日温度 单调栈

--- src/DailyTemperatures.java
+++ src/DailyTemperatures.java
@@ -1,15 +1,36 @@
+import java.util.Stack;
+
 /**
  * @Author Liuxin
  * @Date 2019/4/3 18:53
  *
  * 每日温度
  * https://leetcode-cn.com/problems/daily-temperatures/comments/
+ *
+ * 单调栈
  */
 public class DailyTemperatures {
 
     public static int[] dailyTemperatures(int[] T) {
-        int count = T.length;
-        int cur, wait = 0;
+        int[] res = new int[T.length];
+        Stack<Integer> stack = new Stack<>();
+        int cur, idx;
+
+        for (int i = 0; i < T.length; i++) {
+            cur = T[i];
+
+            while (!stack.isEmpty() && cur > T[stack.peek()]) {
+                idx = stack.pop();
+                res[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+
+        return res;
+
+        // 暴力 O(n * n)
+        /*int count = T.length;
+        int cur, wait;
 
         int[] res = new int[count];
         for (int i = 0; i < count; i++){
@@ -26,11 +47,11 @@ public class DailyTemperatures {
             }
         }
 
-        return res;
+        return res;*/
     }
 
     public static void main(String[] args) {
-        int[] T = new int[]{55,38,53,81,61,93,97,32,43,78};
+        int[] T = new int[]{73,74,75,71,69,72,76,73};
         int[] res = dailyTemperatures(T);
         for (int i : res){
             System.err.println(i);
