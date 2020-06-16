4c7632c42595128e2b269e11f4f5b59934c82a4e
bwt <zonheng@yahoo.com>
2020-06-16 11:18:59 +0800

    两数相除 todo

--- src/Divide.java
+++ src/Divide.java
@@ -4,16 +4,59 @@
  */
 public class Divide {
 
+    /**
+     *
+     * @param dividend 被除数
+     * @param divisor  除数
+     * @return
+     */
     private static int divide(int dividend, int divisor) {
-        return recursive(dividend, divisor, 1);
-    }
+        boolean dividendFlag = true;
+        if (dividend < 0) {
+            dividendFlag = false;
+            dividend = -dividend;
+        }
+        boolean divisorFlag = true;
+        if (divisor < 0) {
+            divisorFlag = false;
+            divisor = -divisor;
+        }
 
-    private static int recursive(int devidend, int devisor, int base) {
-        return 1;
+        // 1. 确定大致范围
+        int res = 1;
+        int product = divisor;
+        while (product <= dividend) {
+            if (product == dividend) {
+                return (dividendFlag ^ divisorFlag) ? -res : res;
+            }
+
+            res <<= 1;
+            product = res * divisor;
+        }
+
+        // 2. 二分查找
+        int left = res >> 1, right = res;
+        int mid;
+
+        while (left + 1 < right) {
+            mid = left + (right - left) / 2;
+            product = mid * divisor;
+
+            if (product < dividend) {
+                left = mid;
+            } else if (product > dividend) {
+                right = mid;
+            } else {
+                left = mid;
+                break;
+            }
+        }
+
+        return (dividendFlag ^ divisorFlag) ? -left : left;
     }
 
     public static void main(String[] args) {
-        divide(100, 3);
+        System.err.println(divide(-2147483648, -1));;
     }
 
 }
