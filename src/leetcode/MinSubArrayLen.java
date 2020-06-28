2cf9a8f7e00d6f708218e00f70f6f2c015796a0a
bwt <zonheng@yahoo.com>
2020-06-28 10:05:25 +0800

    Enhancement: 长度最小的子数组

--- src/leetcode/MinSubArrayLen.java
+++ src/leetcode/MinSubArrayLen.java
@@ -1,32 +1,44 @@
 package leetcode;
 
 /**
+ * 长度最小的子数组
+ * https://leetcode-cn.com/problems/minimum-size-subarray-sum/
+ *
+ * 使用 前缀和 + 双指针
+ *
  * @Author Liuxin
  * @Date 2019/4/1 17:26
  */
 public class MinSubArrayLen {
 
     public static int minSubArrayLen(int s, int[] nums) {
-        int len = 0, sum = 0;
-        int curLen = 0;
+        int start = 0, end = 0;
+        int sum = 0;
+        Integer minLen = null;
+        boolean expand = false;
 
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            curLen++;
+        while (start >= 0 && end < nums.length) {
+            if (start != end) {
+                sum = expand ? sum + nums[end] : sum - nums[start - 1];
+            } else {
+                sum = nums[start];
+            }
 
             if (sum >= s) {
-                len = Math.max(len, curLen);
-                curLen = 0;
-                sum = 0;
-                continue;
+                minLen = minLen != null ? Math.min(minLen, end - start + 1) : end - start + 1;
+                start++;
+                expand = false;
+            } else {
+                end++;
+                expand = true;
             }
         }
 
-        return len;
+        return minLen == null ? 0 : minLen;
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{1,2,3,4,5};
-        System.err.println(minSubArrayLen(11, nums));
+        int[] nums = new int[]{1,1};
+        System.err.println(minSubArrayLen(3, nums));
     }
 }
