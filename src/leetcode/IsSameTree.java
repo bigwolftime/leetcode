3fc7abacaa5495fff088c48ea88ac9d7bdd65074
liuxin <liuxin@weli.cn>
2020-08-07 20:28:03 +0800

    misc: 相同的树 新增解法

--- src/leetcode/IsSameTree.java
+++ src/leetcode/IsSameTree.java
@@ -9,12 +9,13 @@ import utils.TreeNode;
  * https://leetcode-cn.com/problems/same-tree/description/
  * @author bwt
  *
- *	思路：
- *	使用循环，使用辅助栈，前序遍历二叉树。
+ * 思路：
+ * 1. 使用循环，使用辅助栈，前序遍历二叉树。
+ * 2. 使用递归
  */
 public class IsSameTree {
 
-	public static boolean isSameTree(TreeNode p, TreeNode q) {
+	private static boolean isSameTree(TreeNode p, TreeNode q) {
 		Stack<TreeNode> pStack = new Stack<>();
 		Stack<TreeNode> qStack = new Stack<>();
 		
@@ -44,6 +45,24 @@ public class IsSameTree {
 		
         return true;
     }
+
+	private static boolean isSameTree1(TreeNode p, TreeNode q) {
+		if (p == null && q != null) {
+			return false;
+		} else if (p != null && q == null) {
+			return false;
+		}
+
+		if (p == null && q == null) {
+			return true;
+		}
+
+		if (p.val != q.val) {
+			return false;
+		}
+
+		return isSameTree1(p.left, q.left) && isSameTree1(p.right, q.right);
+	}
 	
 	public static void main(String[] args) {
 		TreeNode p = new TreeNode(1);
