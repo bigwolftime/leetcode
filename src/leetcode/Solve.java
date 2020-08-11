b8a85cf256984d4c0e627e1b37463f8f5f3041cb
bwt <zonheng@yahoo.com>
2020-08-12 00:18:39 +0800

    Enhancement: 被围绕的区域 染色+DFS

--- src/leetcode/Solve.java
+++ src/leetcode/Solve.java
@@ -7,23 +7,37 @@ package leetcode;
  * 与边界的 O 直接相连或者间接相连的 O 都不能转化为 X. 其他的 O 可以
  * 使用 染色 + DFS 的办法.
  *
+ * 染色完成后， 可以把 ‘O’ 变成 ‘X’
+ *
+ * 最后把 染色的元素还原即可
+ *
  * @author liuxin
  * @time 2020/8/11 21:02
  */
 public class Solve {
 
     private static void solve(char[][] board) {
-        int len = board.length;
+        int xLen = board.length, yLen;
+        if (board == null || xLen == 0 || (yLen = board[0].length) == 0) {
+            return;
+        }
+
         // 感染
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < yLen; i++) {
             dfs(board, 0, i, 0, i);
         }
-        for (int i = 1; i < len; i++) {
-            dfs(board, i, len - 1, i, len - 1);
+        for (int i = 1; i < xLen; i++) {
+            dfs(board, i, yLen - 1, i, yLen - 1);
         }
-        for (int i = len)
+        for (int i = yLen - 2; i >= 0; i--) {
+            dfs(board, xLen - 1, i, xLen - 1, i);
+        }
+        for (int i = xLen - 2; i > 0; i--) {
+            dfs(board, i, 0, i, 0);
+        }
 
-             //& 置换
+        // 置换
+        change(board);
 
         // 重置
         restore(board);
@@ -35,7 +49,7 @@ public class Solve {
             return;
         }
 
-        if (board[x][y] == 'X') {
+        if (board[x][y] == 'X' || board[x][y] == 'Z') {
             // X 无需处理
             return;
         }
@@ -53,9 +67,19 @@ public class Solve {
         dfs(board, x, y + 1, x, y);
     }
 
+    private static void change(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
     private static void restore(char[][] board) {
-        for (int i = 0; i < board[0].length; i++) {
-            for (int j = 0; j < board.length; j++) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
                 if (board[i][j] == 'Z') {
                     board[i][j] = 'O';
                 }
@@ -65,10 +89,10 @@ public class Solve {
 
     public static void main(String[] args) {
         char[][] board = new char[][]{
-                { 'X', 'X', 'X', 'X' },
-                { 'X', 'X', 'O', 'X' },
-                { 'X', 'X', 'O', 'X' },
-                { 'X', 'X', 'O', 'X' }
+                { 'X', 'O', 'X', 'O', 'X', 'O' },
+                { 'O', 'X', 'O', 'X', 'O', 'X' },
+                { 'X', 'O', 'X', 'O', 'X', 'O' },
+                { 'O', 'X', 'O', 'X', 'O', 'X' }
         };
 
         solve(board);
