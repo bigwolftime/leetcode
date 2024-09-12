ac46924ad41ac2f37b97f5f804a5417d672aa3dd
liuxin <liuxin@xueqiu.com>
2024-09-12 13:46:29 +0800

    misc: 合并两个有序数组-逆向双指针

--- src/leetcode/MergeOrderedArray.java
+++ src/leetcode/MergeOrderedArray.java
@@ -42,10 +42,64 @@ public class MergeOrderedArray {
     }
 
 
+    /**
+     * 逆向双指针
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public static void merge2(int[] nums1, int m, int[] nums2, int n) {
+        if (n == 0) {
+            return;
+        }
+
+        int p = m - 1;
+        int q = n - 1;
+        int curIdx = m + n - 1;
+
+        while (p >= 0 && q >= 0) {
+            int val1 = nums1[p];
+            int val2 = nums2[q];
+
+            if (val1 > val2) {
+                nums1[curIdx] = val1;
+                p--;
+            } else if (val1 < val2) {
+                nums1[curIdx] = val2;
+                q--;
+            } else {
+                // 两个值相等，取其中一个
+                nums1[curIdx] = val1;
+                p--;
+            }
+
+            curIdx--;
+        }
+
+        while (p >= 0) {
+            nums1[curIdx] = nums1[p];
+            p--;
+            curIdx--;
+        }
+        while (q >= 0) {
+            nums1[curIdx] = nums2[q];
+            q--;
+            curIdx--;
+        }
+    }
+
+
+
     public static void main(String[] args) {
         int[] nums1 = new int[]{1,2,3,0,0,0};
         int[] nums2 = new int[]{2,5,6};
 
         merge(nums1, 3, nums2, 3);
+
+
+        int[] nums3 = new int[]{0};
+        int[] nums4 = new int[]{1};
+        merge2(nums3, 0, nums4, 1);
     }
 }
